884a8a35917c4664a41475db72eb9ab385f3781f
Nazar910 <pyvovarnazar@gmail.com>
2017-03-29 18:43:32 +0300

    using lodash get&set

--- spec/split.spec.js
+++ spec/split.spec.js
@@ -1,10 +1,10 @@
 'use strict';
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const splitter = require('../split.js');
+const data = require('./data');
 
 describe('Splitter ', () => {
-
-    let splitter = require('../split.js');
     let self;
 
     beforeEach(() => {
@@ -13,20 +13,18 @@ describe('Splitter ', () => {
         };
     });
 
-    let data = require('./data');
     for (const key of Object.keys(data)) {
         it(key, () => {
             const { message, config, results } = data[key];
             splitter.process.call(self, message, config);
             expect(self.emit.callCount).to.equal(results.length);
-            for (let i = 0; i < results.length - 1; i += 2) {
+            for (let i = 0; i < results.length; i++) {
                 expect(self.emit.getCall(i).args[0]).to.equal(results[i][0]);
-                expect(self.emit.getCall(i).args[1].body).to.deep.equal(results[i][1]);
-            }
-            let last = results.length - 1;
-            expect(self.emit.getCall(last).args[0]).to.equal(results[last][0]);
-            if (results[last][1]) {
-                expect(self.emit.getCall(last).args[1]).to.equal(results[last][1]);
+                if (typeof results[i][1] === 'object') {
+                    expect(self.emit.getCall(i).args[1].body).to.deep.equal(results[i][1]);
+                } else if (typeof results[i][1] === 'string') {
+                    expect(self.emit.getCall(i).args[1]).to.equal(results[i][1]);
+                }
             }
         });
     }
--- split.js
+++ split.js
@@ -23,31 +23,6 @@ function isArrayKey(key) {
     return typeof key !== 'number' && key.indexOf(MANY) > 0;
 }
 
-/**
- * Reads a property of the object based on
- * path up to the index
- *
- * @param obj a hash e.g { foo : { bar : 'hasi' } }
- * @param path an array e.g ['foo','bar']
- * @return 'hasi'
- */
-function get(obj, path) {
-    let index = 0;
-    let result = obj;
-    if (typeof path === 'string') {
-        path = path.split('.');
-    }
-    // If empty path
-    if (path.length === 0) {
-        return obj;
-    }
-    while (index < path.length && result) {
-        result = result[path[index]];
-        index++;
-    }
-    return result;
-}
-
 function toSingleKey(key) {
     return key.substr(0, key.lastIndexOf(MANY));
 }
@@ -61,35 +36,6 @@ function toSingleKey(key) {
  */
 function clone(a) {
     return JSON.parse(JSON.stringify(a));
-}
-
-/**
- * Function that set a value for given path
- *
- * @param obj Value e.g. { foo : { bar : 'hasi' } }
- * @param path Path e.g. ['foo','bar']
- * @param value e.g. 'Horst' so that obj will be after { foo : { bar : 'Horst' } }
- * @returns true if value was set
- */
-function set(obj, path, value) {
-    if (typeof path === 'string') {
-        path = path.split('.');
-    }
-    if (path.length === 0) {
-        return null;
-    }
-    let key = _.last(path);
-    let parentPath = path.slice(0, path.length - 1);
-    let parent = get(obj, parentPath);
-    if (!parent) {
-        // value we are trying to set to does not exists
-        return false;
-    } else if (_.isObject(parent)) {
-        parent[key] = value;
-        return true;
-    }
-    // Parent value is not an object
-    return false;
 }
 
 /**
@@ -112,7 +58,7 @@ function doSplit(path, root, index) {
     // Resulting array
     let currentPath = path.slice(0, index + 1);
     // Getting value on the path
-    let array = get(root, currentPath);
+    let array = _.get(root, currentPath.join('.'));
     if (!array) {
         // Can't find next value along the path
         // will just return latest value
@@ -129,7 +75,7 @@ function doSplit(path, root, index) {
             // Clone root
             let result = clone(root);
             // Replace array with one of it's values
-            set(result, currentPath, clone(arrayValue));
+            _.set(result, currentPath, clone(arrayValue));
             // Do next iteration and store results
             let nextResult = doSplit(path, result, index + 1);
             if (nextResult) {
@@ -169,17 +115,15 @@ function splitMessage(message, splitting, splittingLevel) {
 
 
 function checkSplitting(body, splitting, splittingLevel) {
-    let path = splitting.split('.');
-    path = cutByMaxSplittingLevel(path, splittingLevel);
+    const pathChunks = splitting.split('.');
+    const path = cutByMaxSplittingLevel(pathChunks, splittingLevel);
     let result = body;
     let i = 0;
     while (i < path.length && result) {
-        if (_.isPlainObject(result) || _.isArray(result)) {
-            if (_.isArray(result)) {
-                result = result[0][path[i]];
-            } else {
-                result = result[path[i]];
-            }
+        if (_.isArray(result)) {
+            result = result[0][path[i]];
+        } else if (_.isPlainObject(result)) {
+            result = result[path[i]];
         } else {
             break;
         }
@@ -211,7 +155,8 @@ function processAction(msg, conf) {
                 `The given splitting expression "${splitting}" is invalid: there is no such property!`);
         }
     } else {
-        this.emit('error', 'Splitting level must be bigger than 0');
+        this.emit('error',
+            `The given splitting expression "${splitting}" is invalid: splitting level must be bigger than 0`);
     }
 }
 exports.process = processAction;
