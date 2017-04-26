711a96451a96a1d68b86e0205721b206c91370dc
Nazar910 <pyvovarnazar@gmail.com>
2017-04-26 13:46:12 +0300

    removed * from it in test

--- spec/data/index.js
+++ spec/data/index.js
@@ -1,11 +1,11 @@
 'use strict';
 
 module.exports = {
-    'should split array of objects users[*]': require('./splitArrayOfObjects.json'),
-    'should split array of arrays users[*].friends[*]': require('./splitArrayOfArrays.json'),
-    'should split array of arrays of objects users[*].friends[*]': require('./arrayOfArraysOfObjects.json'),
-    'should split array of arrays of primitives userNames[*]': require('./arrayOfArraysOfPrimitives.json'),
-    'should emit only "end" with empty array userNames[*]': require('./nothingToSplit.json'),
-    'should emit error no such property users[*].emails[*]': require('./noSuchPropertySplitting.json'),
-    'should emit array of primitives userNames[*]': require('./arrayOfPrimitives.json')
+    'should split array of objects users': require('./splitArrayOfObjects.json'),
+    'should split array of arrays users[*].friends': require('./splitArrayOfArrays.json'),
+    'should split array of arrays of objects users.friends': require('./arrayOfArraysOfObjects.json'),
+    'should split array of arrays of primitives userNames': require('./arrayOfArraysOfPrimitives.json'),
+    'should emit only "end" with empty array userNames': require('./nothingToSplit.json'),
+    'should emit error no such property users.emails': require('./noSuchPropertySplitting.json'),
+    'should emit array of primitives userNames': require('./arrayOfPrimitives.json')
 };
