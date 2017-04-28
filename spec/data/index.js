360bb432bf7820eda8d4060f1d3465cd5012cb78
Nazar910 <pyvovarnazar@gmail.com>
2017-04-28 17:41:23 +0300

    refactored tests

--- spec/data/index.js
+++ spec/data/index.js
@@ -7,7 +7,7 @@ module.exports = {
     'should split array of arrays of primitives userNames': require('./arrayOfArraysOfPrimitives.json'),
     'should emit only "end" with empty array userNames': require('./nothingToSplit.json'),
     'should emit error no such property users.emails': require('./noSuchPropertySplitting.json'),
-    'should emit array of primitives userNames': require('./arrayOfPrimitives.json'),
-    'should wrap to { value: "value" } when users is primitive': require('./primitiveValue.json'),
+    'should emit error for array of primitives userNames': require('./arrayOfPrimitives.json'),
+    'should emit error when users is primitive': require('./primitiveValue.json'),
     'should emit object when users is object': require('./objectValue.json')
 };
