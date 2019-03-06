9c75e5a9f02569c378d7fa2f844bebcd6ea2e3e5
ElBabbler <gladikov.denis@gmail.com>
2019-03-06 17:55:18 +0200

    Upgrading sailor: adding async-await emit call to process 'drain' event.

--- split.js
+++ split.js
@@ -4,7 +4,7 @@ const _ = require('lodash');
 const messages = require('elasticio-node').messages;
 
 /** @this processAction */
-function processAction(msg, conf) {
+async function processAction(msg, conf) {
     const splitting = conf.splitter || {};
     let body = msg.body || {};
 
@@ -14,17 +14,17 @@ function processAction(msg, conf) {
     const split = splitting === '$' ? body : _.get(body, splitting);
 
     if (!split) {
-        this.emit('error', new Error(`Could not find properties by following path: "${splitting}"!`));
+        await this.emit('error', new Error(`Could not find properties by following path: "${splitting}"!`));
         return;
     }
 
     if (!_.isObject(split)) {
-        this.emit('error', new Error('Only objects are accepted!'));
+        await this.emit('error', new Error('Only objects are accepted!'));
         return;
     }
 
     if (_.isArray(split) && _.find(split, elem => !_.isObject(elem))) {
-        this.emit('error', new Error('Splitting arrays of objects only!'));
+        await this.emit('error', new Error('Splitting arrays of objects only!'));
         return;
     }
 
@@ -37,9 +37,10 @@ function processAction(msg, conf) {
         result.push(split);
     }
 
-    result.forEach(elem => {
-        this.emit('data', messages.newMessageWithBody(elem));
+    result.forEach(async elem => {
+        await this.emit('data', messages.newMessageWithBody(elem));
     });
-    this.emit('end');
+    await this.emit('end');
 }
+
 exports.process = processAction;
