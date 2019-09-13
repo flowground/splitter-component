632b0413f16b9a42ccf5eb8776a225cd0a749de1
David Zhou <david.zhou@elastic.io>
2019-09-13 17:23:15 +0200

    Implement Split on JSONata Expression and add tests

---
+++ spec/splitOnJsonata/splitOnJsonata.spec.js
@@ -0,0 +1,45 @@
+'use strict';
+const expect = require('chai').expect;
+const sinon = require('sinon');
+const splitter = require('../../lib/actions/splitOnJsonata');
+const data = require('./data');
+
+describe('Split on JSONata ', () => {
+    let self;
+
+    beforeEach(() => {
+        self = {
+            emit: sinon.spy()
+        };
+    });
+
+    for (const key of Object.keys(data)) {
+        it(key, async () => {
+            const { message, config, results } = data[key];
+            await splitter.process.call(self, message, config);
+            for (let i = 0; i < results.length; i++) {
+                const args = self.emit.getCall(i).args;
+
+                const expectedEventType = results[i][0];
+                const actualEventType = args[0];
+                expect(actualEventType).to.equal(expectedEventType);
+
+                const expected = results[i][1];
+                if (!expected) {
+                    continue;
+                }
+
+                let actual = expectedEventType === 'data'
+                    ? args[1].body
+                    : args[1];
+
+                if (expectedEventType === 'error') {
+                    expect(actual).instanceOf(Error);
+                    actual = actual.message;
+                }
+
+                expect(actual).to.deep.equal(expected);
+            }
+        });
+    }
+});
